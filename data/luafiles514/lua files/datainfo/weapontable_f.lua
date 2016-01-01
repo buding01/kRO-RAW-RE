7dda89123ab571042537b5c9a4b43d937cdef33e
Dastgir <dastgirpojee@rocketmail.com>
2016-01-01 05:09:04 -0500

    2012-05-23rdata_j.gpf

--- data/luafiles514/lua files/datainfo/weapontable_f.lua
+++ data/luafiles514/lua files/datainfo/weapontable_f.lua
@@ -9,6 +9,28 @@ function ReqWeaponName(index)
     return ""
   end
 end
+function ReqWeaponNameByClassNum(classNum)
+  if classNum == nil then
+    return ""
+  end
+  local NameMsg = WeaponNameTable[classNum]
+  if NameMsg ~= nil then
+    return NameMsg
+  else
+    return ""
+  end
+end
+function GetRealWeaponId(weaponId)
+  if weaponId == nil then
+    return 0
+  end
+  local id = Expansion_Weapon_IDs[weaponId]
+  if id ~= nil then
+    return id
+  else
+    return weaponId
+  end
+end
 function ReqWeaponHitWaveName(index)
   if WeaponHitWaveNameTable == nil then
     return ""
