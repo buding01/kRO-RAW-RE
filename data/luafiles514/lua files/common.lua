0cf09e4949dc85b3d457e70b8d0a2af86f2b281e
Dastgir <dastgirpojee@rocketmail.com>
2016-01-01 04:49:44 -0500

    2012-01-04rdata_j.gpf

--- data/luafiles514/lua files/common.lua
+++ data/luafiles514/lua files/common.lua
@@ -19,3 +19,22 @@ function getTableSize(t)
   end
   return size
 end
+function GetTableSizeForC(tableName)
+  local t = _G[tableName]
+  local size = 0
+  if t ~= nil then
+    for _, _ in pairs(t) do
+      size = size + 1
+    end
+  end
+  return size
+end
+function getHelpMsg(idx)
+  if HelpMsgStr == nil then
+    return ""
+  end
+  if HelpMsgStr[idx] == nil then
+    return ""
+  end
+  return HelpMsgStr[idx]
+end
